8f256acd40327f09abb34a31acbbe50e376c6909
Angie Li <liangelale@gmail.com>
2019-05-30 11:01:22 -0500

    Adding a new date to welcome alert

---
+++ hello.js
@@ -0,0 +1,9 @@
+// prints a greeting to the console.
+/*
+console.log("foobar")
+console.log("racecar")
+console.log("hello, world!", "what's up?")*/
+
+let firstName = "Angie"
+let lastName = "Li"
+firstName + " " + lastName
